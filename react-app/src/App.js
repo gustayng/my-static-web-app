38d2de3d8e0abef9c742eeb955816d4c6138d044
Yngve Gustafsson <Yngve.Gustafsson@husqvarnagroup.com>
2023-06-28 21:44:03 +0200

    Playing

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <button>Press Me</button>
         <HeaderBar />
         <div className="section columns">
           <NavBar />
